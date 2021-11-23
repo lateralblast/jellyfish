3fb1526f8db801ddfcce68f46c58533e194396a3
Richard Spindler <richard@lateralblast.com.au>
2021-11-24 10:23:30 +1100

    Initial git repository commit

---
+++ README.md
@@ -0,0 +1,28 @@
+![alt tag](https://raw.githubusercontent.com/lateralblast/jellyfish/master/jellyfish.jpg)
+
+JELLYFISH
+=========
+
+Jellyfish is a script to process the VMware HCL JSON file produced here:
+
+https://www.virten.net/2017/01/vmware-io-devices-hcl-in-json-format/
+
+Requirements
+------------
+
+Required Python modules:
+
+- json
+
+License
+-------
+
+This software is licensed as CC-BA (Creative Commons By Attrbution)
+
+http://creativecommons.org/licenses/by/4.0/legalcode
+
+Usage
+-----
+
+Examples
+--------
---
+++ jellyfish.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+# Name:         jellyfish
+# Version:      0.0.0
+# Release:      1
+# License:      CC-BA (Creative Commons By Attrbution)
+#               http://creativecommons.org/licenses/by/4.0/legalcode
+# Group:        System
+# Source:       N/A
+# URL:          http://lateralblast.com.au/
+# Distribution: UNIX
+# Vendor:       Lateral Blast
+# Packager:     Richard Spindler <richard@lateralblast.com.au>
+# Description:  Python script to process the VMware HCL JSON file produced here:
+#               https://www.virten.net/2017/01/vmware-io-devices-hcl-in-json-format/
+
+# Import modules
+
+import json
